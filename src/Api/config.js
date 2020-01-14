1ac6c19c4dbd1ade00f9c81ca1036aff40993cfb
piyushtnk <piyushtnk11@gmail.com>
2020-01-14 16:58:06 +0700

    Dir fixing Api folder

--- src/Api/config.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-const config = axios.create({
-	baseURL: 'http://119.8.40.98/api',
-	headers: {
-		Authorization: `Bearer ${localStorage.getItem('refreshToken')}`
-	}
-});
-export default config;
