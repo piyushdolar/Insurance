fb8bc1a10d6ce41399104248a3add4aa3db615ce
piyushtnk <piyushtnk11@gmail.com>
2020-01-30 16:08:40 +0700

    Https changes 1

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './authHeader';
 const config = axios.create({
-	baseURL: 'http://119.8.40.98/api',
+	baseURL: 'https://lcpi.la/api',
 	headers: authHeader()
 });
 export default config;
